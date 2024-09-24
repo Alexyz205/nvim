46474ecfeca2eedb734fdb991e80ba4c6b79d2ac
Alexyz205 <anathos205@gmail.com>
2024-09-24 22:28:48 +0200

    [lspconfig] add automatic setup of lsp installed by Mason

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -1,22 +1,16 @@
 -- load lspconfig
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
-
--- EXAMPLE
-local servers = { "lua_ls", "ltex" }
-
--- lsps with default config
-local nvlsp = require "nvchad.configs.lspconfig"
-for _, lsp in ipairs(servers) do
-    lspconfig[lsp].setup {
-        on_attach = nvlsp.on_attach,
-        on_init = nvlsp.on_init,
-        capabilities = nvlsp.capabilities
-    }
-end
+-- List of servers installed by default
+local servers = { "lua_ls", "ltex", "pyright" }
 
 require("mason-lspconfig").setup {
     automatic_installation = true,
     ensure_installed = servers
 }
+
+require("mason-lspconfig").setup_handlers({
+    function(server_name) -- Default handler for any LSP server
+        require("lspconfig")[server_name].setup {}
+    end,
+})
