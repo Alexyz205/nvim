05bc973ec89f673a4a5a026376a679ccdb2679c5
Alexyz205 <anathos205@gmail.com>
2025-02-26 19:50:17 +0100

    [nvim-tree] add git support

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -3,12 +3,22 @@ require("nvim-tree").setup({
         sorter = "case_sensitive"
     },
     view = {
-        width = 30
+        width = 30,
+        side = "right"
     },
     renderer = {
-        group_empty = true
+        group_empty = true,
+        highlight_git = true,
+        icons = {
+            show = {
+                git = true
+            }
+        }
     },
     filters = {
         dotfiles = true
+    },
+    git = {
+        enable = true
     }
 })
