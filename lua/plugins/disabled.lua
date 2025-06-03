dc68f69c73b402dc9b9080b964950dfbc4063543
Alexyz205 <anathos205@gmail.com>
2025-06-03 09:44:20 +0200

    [yazi] replace snack explorer by yazi

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,4 +3,11 @@ return {
   { "akinsho/bufferline.nvim", enabled = false },
   -- disable tokyonight
   { "folke/tokyonight.nvim",   enabled = false },
+  -- disable snack explorer
+  {
+    "folke/snacks.nvim",
+    opts = {
+      explorer = { enabled = false },
+    },
+  },
 }
